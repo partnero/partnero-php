a11d2930c403781fa5176f1570ffdbafe97284bc
BrijeshTRC <brijesh@partnero.com>
2023-09-14 21:20:16 +0530

    Re-adding balance & credit for customers

--- src/Endpoints/Customers.php
+++ src/Endpoints/Customers.php
@@ -5,10 +5,11 @@ declare(strict_types=1);
 namespace Partnero\Endpoints;
 
 use JsonException;
-use Partnero\Exceptions\RequestException;
-use Partnero\Models\Customer;
 use Partnero\Models\Partner;
+use Partnero\Models\Customer;
 use Partnero\Models\Transaction;
+use Partnero\Models\BalanceCredit;
+use Partnero\Exceptions\RequestException;
 use Psr\Http\Client\ClientExceptionInterface;
 
 class Customers extends AbstractEndpoint
@@ -132,5 +133,38 @@ class Customers extends AbstractEndpoint
         }
 
         return $this->call('post', $data, $this->getEndpointUri() . ':invite');
+    }
+
+    /**
+     * @param string|Customer $key
+     * @return array
+     * @throws ClientExceptionInterface
+     * @throws JsonException
+     * @throws RequestException
+     */
+    public function balance(string|Customer $key): array
+    {
+        return $this->call(
+            'get',
+            [],
+            $this->getEndpointUri() . '/' . $key . '/balance'
+        );
+    }
+
+    /**
+     * @param string|Customer $key
+     * @param array|BalanceCredit $credit
+     * @return array
+     * @throws ClientExceptionInterface
+     * @throws JsonException
+     * @throws RequestException
+     */
+    public function credit(string|Customer $key, array|BalanceCredit $credit): array
+    {
+        return $this->call(
+            'post',
+            $this->modelData($credit),
+            $this->getEndpointUri() . '/' . $key . '/balance/credit'
+        );
     }
 }
